c8fba99b9ffe041fc8128a1626063aecdda231ac
Justin Silvestre <justinsilvestre@gmail.com>
2015-04-06 15:11:13 -0500

    heroku tweak

--- app.js
+++ app.js
@@ -5,9 +5,17 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+
+/*
 var mongo = require('mongodb');
 var monk = require('monk');
 var db = monk('localhost:27017/nodetest1');
+*/
+
+var mongoUri = process.env.MONGOLAB_URI || 'localhost:27017/nodetest1';
+var monk = require('monk');
+var db = monk(mongoUri);
+
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
